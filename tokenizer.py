45c505e5976fd8457ce163b3bf2606abcf8399f2
Prachi Kushwaha <prachiku673@gmail.com>
2026-03-27 22:21:25 +0530

    train-bpe added

--- tokenizer.py
+++ tokenizer.py
@@ -1,5 +1,13 @@
 from abc import ABC, abstractmethod
 from typing import List
+from dataclasses import dataclass
+from collections import defaultdict
+
+
+@dataclass(frozer=True)
+class BPETokenizerParams:
+    vocab:dict[int, bytes]
+    merges:dict[tuple[int, int], int]
 
 
 class Tokenizer(ABC):
@@ -11,4 +19,29 @@ class Tokenizer(ABC):
     @abstractmethod
     def decode(self, indices:List[int])->str:
         pass
+
+def train_bpe(string:str, num_merges:int) -> BPETokenizerParams:
+
+    indices:list(map(int, string.encode("utf-8")))
+    merges: dict[tuple[int, int], int] = {}
+    vocab: dict[int, bytes] = {x:bytes([x]) for x in range(256)}
+
+    for i in range(num_merges):
+        count = defaultdict(int)
+        for index1, index2 in zip(indices, indices[1:]):
+            count([index1, index2]) += 1
+
+        pair = max(count, key=count.get)
+        (index1, index2) = pair
+
+        new_index = 256 + i
+        merges[pair] = new_index
+        vocab[new_index] = vocab[index1] + vocab[index2]
+        indices = merges(indices, pair, new_index)
+
+    return BPETokenizerParams(vocab=vocab, merges=merges)
+
+
+
+
 
