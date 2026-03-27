44c354663de4c07971a9d80551602822ce3399c7
Prachi Kushwaha <prachiku673@gmail.com>
2026-03-27 22:29:29 +0530

    merges and bpetokenizerclass added

--- tokenizer.py
+++ tokenizer.py
@@ -3,13 +3,6 @@ from typing import List
 from dataclasses import dataclass
 from collections import defaultdict
 
-
-@dataclass(frozer=True)
-class BPETokenizerParams:
-    vocab:dict[int, bytes]
-    merges:dict[tuple[int, int], int]
-
-
 class Tokenizer(ABC):
 
     @abstractmethod
@@ -19,6 +12,26 @@ class Tokenizer(ABC):
     @abstractmethod
     def decode(self, indices:List[int])->str:
         pass
+
+
+@dataclass(frozer=True)
+class BPETokenizerParams:
+    vocab:dict[int, bytes]
+    merges:dict[tuple[int, int], int]
+
+def merges(indices: List[int], pair: tuple[int, int], new_index: int) ->List[int]:
+    """ Return updated indices by updating with all instances of `pair` replaced with `new_index`. """
+    new_indices = []
+    i = 0
+    while i < len(indices):
+        if i + 1 < len(indices) and indices[i] == pair[0] and indices[i + 1] == pair[1]:
+            new_indices.append(new_index)
+            i += 2
+        else:
+            new_indices.append(indices[i])
+            i += 1
+    return new_indices
+
 
 def train_bpe(string:str, num_merges:int) -> BPETokenizerParams:
 
@@ -40,6 +53,21 @@ def train_bpe(string:str, num_merges:int) -> BPETokenizerParams:
         indices = merges(indices, pair, new_index)
 
     return BPETokenizerParams(vocab=vocab, merges=merges)
+
+class BPETokenizer(Tokenizer):
+    """BPE tokenizer given a set of merges and a vocabulary."""
+    def __init__(self, params: BPETokenizerParams):
+        self.params = params
+    def encode(self, string: str) -> list[int]:
+        indices = list(map(int, string.encode("utf-8")))  # @inspect indices
+        # Note: this is a very slow implementation
+        for pair, new_index in self.params.merges.items():  # @inspect pair, @inspect new_index
+            indices = merges(indices, pair, new_index)
+        return indices
+    def decode(self, indices: list[int]) -> str:
+        bytes_list = list(map(self.params.vocab.get, indices))  # @inspect bytes_list
+        string = b"".join(bytes_list).decode("utf-8")  # @inspect string
+        return string
 
 
 
