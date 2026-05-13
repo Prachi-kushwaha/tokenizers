824ef6c2266675957adb250abc1fd868ddff43f3
Prachi Kushwaha <prachiku673@gmail.com>
2026-05-13 09:38:44 +0530

    full transformer block

--- pre_tokenizer/models/transformers/transformers.py
+++ pre_tokenizer/models/transformers/transformers.py
@@ -1,10 +1,10 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 
 import math
 
-class PE(nn.Module):
+class PositionalEncoding(nn.Module):
+
     def __init__(self, hidden_size, seq_len):
         super().__init__()
 
@@ -20,72 +20,134 @@ class PE(nn.Module):
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
 
-        pe = pe.unsqueeze(0)  # (1, S, D)
+        pe = pe.unsqueeze(0)
 
         self.register_buffer("pe", pe)
 
     def forward(self, x):
-        """
-        x shape = (B, S, D)
-        """
+
         S = x.size(1)
 
         return x + self.pe[:, :S, :]
+class MultiHeadAttention(nn.Module):
 
-
-
-class transformerAttention(nn.Module):
-    def __init__(self, vocab_size, seq_len, hidden_size, head_no):
+    def __init__(self, hidden_size, num_heads):
         super().__init__()
 
-        self.vocab_size = vocab_size
-        self.seq_len = seq_len
+        assert hidden_size % num_heads == 0
+
         self.hidden_size = hidden_size
-        self.head_no = head_no
-        assert hidden_size % head_no == 0
-        self.head_dim = hidden_size // head_no
-        self.qkv_proj = nn.Linear(hidden_size, 3*hidden_size, bias = False)
+        self.num_heads = num_heads
+        self.head_dim = hidden_size // num_heads
+
+        self.qkv_proj = nn.Linear(hidden_size, 3 * hidden_size)
+
         self.out_proj = nn.Linear(hidden_size, hidden_size)
-        self.embedd = nn.Embedding(vocab_size, hidden_size)
-        self.norm = nn.LayerNorm(hidden_size)
-
-        self.pe = PE(hidden_size, seq_len)
-
-        # qkv projection
-        self.qkv_proj = nn.Linear(
-            hidden_size,
-            3 * hidden_size,
-            bias=False
-        )
-
 
     def forward(self, x):
-        B, S = x.shape
-        token_emb = self.embedd(x)
 
-        # apply positional encoding
-        x = self.pe(token_emb)
+        B, S, D = x.shape
 
-        # qkv projection
         qkv = self.qkv_proj(x)
 
-        # split q,k,v
         q, k, v = torch.chunk(qkv, 3, dim=-1)
-        q = q.view(B, S, self.head_no, self.head_dim).transpose(1, 2)
-        k = k.view(B, S, self.head_no, self.head_dim).transpose(1, 2)
-        v = v.view(B, S, self.head_no, self.head_dim).transpose(1, 2)
 
-        attention_score = q @ (k.transpose(-2,-1))
+        q = q.view(B, S, self.num_heads, self.head_dim).transpose(1, 2)
 
-        attention_score = attention_score / math.sqrt(self.head_dim)
-        attention_score = torch.softmax(attention_score, dim=-1)
-        attention_score = attention_score @ v
+        k = k.view(B, S, self.num_heads, self.head_dim).transpose(1, 2)
 
-        out = attention_score.transpose(1, 2).contiguous()
+        v = v.view(B, S, self.num_heads, self.head_dim).transpose(1, 2)
 
-        out = out.view(B, S, self.hidden_size)
+        attention = q @ k.transpose(-2, -1)
+
+        attention = attention / math.sqrt(self.head_dim)
+
+        attention = torch.softmax(attention, dim=-1)
+
+        out = attention @ v
+
+        out = out.transpose(1, 2).contiguous()
+
+        out = out.view(B, S, D)
+
         out = self.out_proj(out)
-        out = self.norm(x + out)
+
         return out
 
-    
+class FeedForward(nn.Module):
+
+    def __init__(self, hidden_size, mlp_dim):
+        super().__init__()
+
+        self.net = nn.Sequential(
+            nn.Linear(hidden_size, mlp_dim),
+            nn.GELU(),
+            nn.Linear(mlp_dim, hidden_size)
+        )
+
+    def forward(self, x):
+
+        return self.net(x)
+
+
+class TransformerBlock(nn.Module):
+
+    def __init__(self, hidden_size, num_heads, mlp_dim):
+        super().__init__()
+
+        self.norm1 = nn.LayerNorm(hidden_size)
+
+        self.attn = MultiHeadAttention(
+            hidden_size,
+            num_heads
+        )
+
+        self.norm2 = nn.LayerNorm(hidden_size)
+
+        self.mlp = FeedForward(
+            hidden_size,
+            mlp_dim
+        )
+
+    def forward(self, x):
+
+        # attention block
+        x = x + self.attn(self.norm1(x))
+
+        # feedforward block
+        x = x + self.mlp(self.norm2(x))
+
+        return x
+
+
+class Transformer(nn.Module):
+
+    def __init__(self, vocab_size, seq_len, hidden_size, num_heads, mlp_dim, num_layers):
+        super().__init__()
+
+        self.embedding = nn.Embedding(vocab_size, hidden_size)
+
+        self.pe = PositionalEncoding(hidden_size, seq_len)
+
+        self.blocks = nn.ModuleList([ TransformerBlock(hidden_size, num_heads,mlp_dim)
+            for _ in range(num_layers)
+        ])
+
+        self.final_norm = nn.LayerNorm(hidden_size)
+
+        self.lm_head = nn.Linear(hidden_size, vocab_size)
+
+    def forward(self, x):
+
+        x = self.embedding(x)
+
+        x = self.pe(x)
+
+        for block in self.blocks:
+            x = block(x)
+
+        x = self.final_norm(x)
+
+        logits = self.lm_head(x)
+
+        return logits
